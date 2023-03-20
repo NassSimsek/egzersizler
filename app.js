6048bb4a34964e59e83c72209512f718c7b3e826
S Nas <nass.simsek@gmail.com>
2023-03-20 10:41:46 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const api_key="1072b2266692040b4c68ab46";
+const url="https://v6.exchangerate-api.com/v6/" + api_key;
+
+//elements
+const currency_one=document.getElementById("currency_one");
+const currency_two=document.getElementById("currency_two")
+const list_one=document.getElementById("list_one");
+const list_two=document.getElementById("list_two");
+const amount=document.getElementById("amount");
+const calculate=document.getElementById("calculate");
+const result=document.getElementById("result");
+const exchange=document.getElementById("exchange");
+
+fetch(url + "/codes")
+.then(res =>res.json())
+.then(data =>{
+    const items=data.supported_codes;
+    let options;
+    for(let item of items){
+        options += `<option value=${item[0]} > ${item[1]} </option>`;
+    }
+    list_one.innerHTML=options;
+    list_two.innerHTML=options;
+})
+
+
+
+calculate.addEventListener("click",function(){
+    const doviz1=currency_one.value;
+    const doviz2=currency_two.value;
+    const miktar=amount.value;
+
+    fetch(url+"/latest/"+doviz1)
+    .then(res=>res.json())
+    .then(data =>{
+        const sonuc=(data.conversion_rates[doviz2]*miktar).toFixed(4);
+        result.innerHTML=`
+         <div class="card border-primary">
+                    <div class="card-body text-center" style="font-size:30px;">
+                     ${miktar} ${doviz1}=${sonuc} ${doviz2}
+                    </div>
+                </div> 
+        `;
+    })
+})
+
+exchange.addEventListener("click",()=>{
+    let cur_one=currency_one.value;
+    currency_one.value=currency_two.value;
+    currency_two.value=cur_one;
+})
